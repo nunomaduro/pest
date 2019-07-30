61bb3c499c7325c2a51e4326322fa92f886f8cad
Jibé Barth <barth.jib@gmail.com>
2019-07-30 21:22:25 +0200

    Add phpinsights config

---
+++ phpinsights.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+
+    'preset' => 'default',
+    'exclude' => [
+        'phpinsights.php',
+        'src/bootstrap.php',
+    ],
+    'add' => [],
+    'remove' => [],
+    'config' => [
+        \SlevomatCodingStandard\Sniffs\Functions\StaticClosureSniff::class => [
+            'exclude' => [
+                'src/Execution.php',
+            ]
+        ]
+    ],
+
+];
